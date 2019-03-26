1189fda68a22ff483401b9c8c2af689932fb1a81
sunjintao123 <1207899652@qq.com>
2019-03-26 11:16:05 +0000

    加入购物车

--- app/Http/Controllers/Goods/GoodsDetailController.php
+++ app/Http/Controllers/Goods/GoodsDetailController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Goods;
 
 use App\Http\Controllers\Controller;
+use App\Model\CartModel;
 use App\Model\GoodsModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
 
 class GoodsDetailController extends Controller
 {
@@ -25,5 +28,30 @@ class GoodsDetailController extends Controller
 
         $info = GoodsModel::where($where)->first();
         return $info;
+    }
+
+
+    public function addCart(Request $request)
+    {
+        $data = [
+            'goods_id'  =>  $request->input('goods_id'),
+            'num'       =>  $request->input('num'),
+            'add_time'  =>  time(),
+            'uid'       =>  $request->input('uid'),
+            'session_token' =>  Redis::hget('str:u:token').$request->input('uid')
+        ];
+        $id = CartModel::insertGetId($data);
+        if($id){
+            $response = [
+                'error' =>  0,
+                'msg'   =>  '添加成功'
+            ];
+        }else{
+            $response = [
+                'error' =>  500005,
+                'msg'   =>  '添加失败'
+            ];
+        }
+        return $response;
     }
 }
