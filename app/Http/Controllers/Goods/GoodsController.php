e45aec5b915a794f9410a940bebfd0a8e672d5a1
xushihao7 <1501024920.com>
2019-03-27 15:46:44 +0800

    缓存

--- app/Http/Controllers/Goods/GoodsController.php
+++ app/Http/Controllers/Goods/GoodsController.php
@@ -1,12 +1,13 @@
 <?php
   namespace App\Http\Controllers\Goods;
   use App\Model\GoodsModel;
+  use Illuminate\Support\Facades\Redis;
   class GoodsController {
       public  function  goodsList(){
           $goodsInfo=GoodsModel::get();
-          $response=[
-              'list'=>$goodsInfo
-          ];
-          return $response;
+          $key="h:goodsInfo";
+          Redis::hSet($key,'goodsInfo',$goodsInfo);
+          Redis::expire($key,83600);
+          return $goodsInfo;
       }
   }
