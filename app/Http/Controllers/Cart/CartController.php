79a80129712bedd55b57c482d0e8282356afa546
sunjintao123 <1207899652@qq.com>
2019-03-27 01:53:04 +0000

    cartgoods

--- app/Http/Controllers/Cart/CartController.php
+++ app/Http/Controllers/Cart/CartController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Cart;
 
 use App\Http\Controllers\Controller;
 use App\Model\CartModel;
+use App\Model\GoodsModel;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -18,6 +19,18 @@ class CartController extends Controller
         ];
         $info = CartModel::where($where)->get();
 
-        return $info;
+        foreach($info as $k=>$v){
+            $where = [
+                'goods_id'  =>  $v['goods_id']
+            ];
+            $goodsInfo = GoodsModel::where($where)->first();
+            var_dump($v);
+            $v['goods_name'] = $goodsInfo->goods_name;
+            $v['price'] = $goodsInfo->price;
+            $v['img'] = $goodsInfo->img;
+            $data[] = $v;
+        }
+
+        return $data;
     }
 }
