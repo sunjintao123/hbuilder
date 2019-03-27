1e13ac767fecc9d51084281510e563d2280448cd
sunjintao123 <1207899652@qq.com>
2019-03-27 08:33:10 +0000

    添加注释

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,22 @@ $router->post('login','Login\LoginController@login');//登录
 
 $router->post('center','Login\LoginController@center');//个人中心
 
-$router->get('/goods/list',"Goods\GoodsController@goodsList");
+$router->get('/goods/list',"Goods\GoodsController@goodsList"); //商品展示
 
-$router->get('/goods/detail/{id}',"Goods\GoodsDetailController@GoodsDetail");
+$router->get('/goods/detail/{id}',"Goods\GoodsDetailController@GoodsDetail"); //商品详情
 
-$router->post('/cart/add',"Goods\GoodsDetailController@addCart");
+$router->post('/cart/add',"Goods\GoodsDetailController@addCart"); //加入购物车
 
-$router->post('/order/list',"Order\OrderController@orderList");
-$router->post('/orderadd',"Order\OrderController@orderAdd");
-$router->get('/orderadd',"Order\OrderController@orderAdd");
+$router->post('/order/list',"Order\OrderController@orderList");//订单展示
+$router->post('/orderadd',"Order\OrderController@orderAdd");//添加订单
+$router->get('/orderadd',"Order\OrderController@orderAdd");//添加订单
 
 
-$router->post('/cart/list',"Cart\CartController@cartList");
+$router->post('/cart/list',"Cart\CartController@cartList");//购物车展示
 
-$router->post('/goods/fav',"Goods\GoodsDetailController@addGoodsFav");
-$router->post('/goods/zan',"Goods\GoodsDetailController@addGoodsZan");
+$router->post('/goods/fav',"Goods\GoodsDetailController@addGoodsFav");//加入收藏
+$router->post('/goods/zan',"Goods\GoodsDetailController@addGoodsZan");//点赞
 
-$router->post('/goods/favlist',"Goods\GoodsDetailController@goodsFav");
-$router->post('/goods/zanlist',"Goods\GoodsDetailController@goodsZan");
+$router->post('/goods/favlist',"Goods\GoodsDetailController@goodsFav");//收藏列表
+$router->post('/goods/zanlist',"Goods\GoodsDetailController@goodsZan");//点赞列表
 
